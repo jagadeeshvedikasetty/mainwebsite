1ebd91817919d515ef5cf7b020199a53797fd121
jagadeeshvedikasetty <jagadeeshvedikasetty@users.noreply.github.com>
2026-09-05 15:05:13 +0530

    feat: add circular rotating text animation around logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -26,9 +26,22 @@ export default function Navbar() {
           {isMenuOpen ? '✕' : '☰'}
         </button>
 
-        <Link href="/" className="logo-link relative">
+        <Link href="/" className="logo-link relative flex items-center justify-center w-40 h-16">
           <div id="hotspot-header-logo-left" className="hotspot-small absolute top-0 -left-6 z-50"></div>
-          <Image src="/logo.png" alt="Janani Home Foods Logo" width={180} height={70} className="logo-img relative z-10" />
+          
+          <svg className="rotating-text absolute z-0 w-32 h-32 text-orange-600" viewBox="0 0 100 100" style={{ pointerEvents: 'none', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
+            <defs>
+              <path id="circlePath" d="M 50, 50 m -40, 0 a 40,40 0 1,1 80,0 a 40,40 0 1,1 -80,0" />
+            </defs>
+            <text fontSize="10.5" fontWeight="bold" letterSpacing="1.8" fill="var(--primary-color)">
+              <textPath href="#circlePath" startOffset="0%">
+                JANANI HOME FOODS • AUTHENTIC TASTE • 
+              </textPath>
+            </text>
+          </svg>
+          
+          <Image src="/logo.png" alt="Janani Home Foods Logo" width={110} height={50} className="logo-img relative z-10" />
+          
           <div id="hotspot-header-logo-right" className="hotspot-small absolute top-0 -right-6 z-50"></div>
         </Link>
 
