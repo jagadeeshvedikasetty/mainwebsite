d639e4f2015250ea9f53006f4cc05cc296dfe01e
jagadeeshvedikasetty <jagadeeshvedikasetty@users.noreply.github.com>
2026-09-07 11:46:43 +0530

    style: remove Lottie and implement elegant custom CSS rotating text around logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,9 +3,6 @@
 import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import dynamic from 'next/dynamic';
-const Lottie: any = dynamic(() => import('lottie-react').then((mod) => mod.Lottie as any), { ssr: false });
-import logoAnimation from '../assets/logo-animation.json';
 import { useCartStore } from '../store/cartStore';
 import './navbar.css';
 
@@ -32,14 +29,19 @@ export default function Navbar() {
         <Link href="/" className="logo-link">
           <div id="hotspot-header-logo-left" className="hotspot-small" style={{ position: 'absolute', top: 0, left: '-24px', zIndex: 50 }}></div>
           
-          {/* @ts-ignore */}
-          <Lottie 
-            src={logoAnimation} 
-            autoplay 
-            loop 
-            className="logo-animation-lottie"
-            style={{ width: '100%', height: '100%' }}
-          />
+          <div className="logo-animation-wrapper">
+            <svg className="rotating-text-svg" viewBox="0 0 100 100">
+              <defs>
+                <path id="circlePath" d="M 50, 50 m -40, 0 a 40,40 0 1,1 80,0 a 40,40 0 1,1 -80,0" />
+              </defs>
+              <text fontSize="12" fontWeight="bold" letterSpacing="2.5" fill="#d97706">
+                <textPath href="#circlePath" startOffset="0%">
+                  JANANI HOME FOODS • AUTHENTIC •
+                </textPath>
+              </text>
+            </svg>
+            <Image src="/logo.png" alt="Janani Home Foods Logo" width={65} height={65} className="logo-img" />
+          </div>
           
           <div id="hotspot-header-logo-right" className="hotspot-small" style={{ position: 'absolute', top: 0, right: '-24px', zIndex: 50 }}></div>
         </Link>
